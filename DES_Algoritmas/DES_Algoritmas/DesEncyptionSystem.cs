45c06d92a58885c6a4af45a97da67099f8d8d90e
Nojus <78691849+NoahIT@users.noreply.github.com>
2024-03-13 20:54:27 +0000

    ini

---
+++ DES_Algoritmas/DES_Algoritmas/DesEncyptionSystem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DES_Algoritmas
+{
+    public class DesEncryptionSystem
+    {
+        public static byte[] EncryptText(string plainText, string key, CipherMode mode)
+        {
+            using (DES des = DES.Create())
+            {
+                des.Mode = mode;
+                des.Key = Encoding.UTF8.GetBytes(key);
+                des.IV = des.Key;
+
+                ICryptoTransform encryptor = des.CreateEncryptor(des.Key, des.IV);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs))
+                        {
+                            sw.Write(plainText);
+                        }
+                    }
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        public static string DecryptText(byte[] cipherText, string key, CipherMode mode)
+        {
+            using (DES des = DES.Create())
+            {
+                des.Mode = mode;
+                des.Key = Encoding.UTF8.GetBytes(key);
+                des.IV = des.Key;
+
+                ICryptoTransform decryptor = des.CreateDecryptor(des.Key, des.IV);
+
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader reader = new StreamReader(cs))
+                        {
+                            return reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void SaveToFile(string filename, byte[] data)
+        {
+            File.WriteAllBytes(filename, data);
+        }
+
+        public static byte[] ReadFromFile(string filename)
+        {
+            return File.ReadAllBytes(filename);
+        }
+    }
+
+}
---
+++ DES_Algoritmas/DES_Algoritmas/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Security.Cryptography;
+
+namespace DES_Algoritmas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DES Sifravimo/Desifravimo sistema:");
+            Console.WriteLine("1: Sifruoti teksta");
+            Console.WriteLine("2: Desifruoti teksta");
+            Console.Write("Pasirinkite opcija: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nIveskite slapta rakta (8 characters): ");
+            string key = Console.ReadLine();
+            if (key.Length != 8)
+            {
+                Console.WriteLine("Raktas turi buti 8 simboliu");
+                return;
+            }
+
+            Console.WriteLine("Pasirinkite sifravimo moda: ");
+            Console.WriteLine("1: ECB");
+            Console.WriteLine("2: CBC");
+            Console.WriteLine("3: CFB");
+            Console.Write("Pasirinkimas: ");
+            int modeChoice = Convert.ToInt32(Console.ReadLine());
+
+            CipherMode mode = modeChoice switch
+            {
+                1 => CipherMode.ECB,
+                2 => CipherMode.CBC,
+                3 => CipherMode.CFB,
+                _ => throw new ArgumentOutOfRangeException("Tokios opcijos nera.")
+            };
+
+            if (choice == 1)
+            {
+
+                Console.Write("\nIveskite teksta kuri norite sifruoti: ");
+                string plainText = Console.ReadLine();
+
+                byte[] encrypted = DesEncryptionSystem.EncryptText(plainText, key, mode);
+                string encryptedText = Convert.ToBase64String(encrypted);
+                Console.WriteLine($"\nEncrypted text: {encryptedText}");
+
+                Console.Write("Ar norite issaugoti sifruota teksta faile? (y/n): ");
+                string save = Console.ReadLine();
+                if (save.ToLower() == "y")
+                {
+                    Console.Write("Iveskite failo pavadinima: ");
+                    string filename = Console.ReadLine();
+                    DesEncryptionSystem.SaveToFile(filename, encrypted);
+                    Console.WriteLine("Sifruotas tekstas issaugotas faile.");
+                    Console.WriteLine($"Faila rasite cia: /DES_Algoritmas/bin/Debug/net8.0/{filename}");
+                    Console.ReadKey();
+                }
+            }
+            else if (choice == 2)
+            {
+                Console.Write("Irasykite sifruota teksta arba failo varda: ");
+                string input = Console.ReadLine();
+                byte[] encryptedData;
+
+                if (File.Exists(input))
+                {
+                    encryptedData = DesEncryptionSystem.ReadFromFile(input);
+                    Console.WriteLine("Sifruotas tekstas perskaitytas is failo.");
+                }
+                else
+                {
+                    encryptedData = Convert.FromBase64String(input);
+                }
+
+                string decryptedText = DesEncryptionSystem.DecryptText(encryptedData, key, mode);
+                Console.WriteLine($"Desifruotas tekstas: {decryptedText}");
+            }
+            else
+            {
+                Console.WriteLine("Tokios opcijos nera.");
+            }
+        }
+    }
+}
+
